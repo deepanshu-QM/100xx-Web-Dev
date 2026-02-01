70e607df540e2d3868ac4df2d4b054cb3f3993b3
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-02 04:11:11 +0530

    Largest value

--- objectArray_assignment/4index.js
+++ objectArray_assignment/4index.js
@@ -2,3 +2,15 @@
 /* Find the Largest Value 
 input : { a: 10, b: 50, c: 20 }
 output :b  */
+
+Input =  { a: 10, b: 50, c: 20 }
+
+let maxValue = null;
+let maxkey = " ";
+for(let key in Input){
+    if(Input[key]> maxValue){
+        maxValue = Input[key];
+        maxkey = key;
+    }
+}
+console.log(maxkey)
