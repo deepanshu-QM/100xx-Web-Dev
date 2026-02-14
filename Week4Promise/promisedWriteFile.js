2485c7eabf9db1cdab87ccbc5d2cb81c51f08352
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-15 01:12:41 +0530

    Correct Promisified Write function

--- Week4Promise/promisedWriteFile.js
+++ Week4Promise/promisedWriteFile.js
@@ -1,22 +1,47 @@
 
-const fs = require('fs');
+// Create Promified Function to write 
+const fs  = require('fs')
 
-function PromisifiedWriteFile(filename,encoding){
+//Read : 
+function PromisifiedReadFile(filename,encoding){
     return new Promise(function(resolve,reject){
-        fs.readFile(function(err,data){
+        fs.readFile(filename,encoding, function(err,data){
             if(err){
                 reject(err);
             }else{
-                
+                resolve(data);
             }
         })
     })
 }
 
-PromisifiedWriteFile("main.txt","utf-8")
-              .then(function(data){
-                console.log(data);
-              })
-              .catch(function(err){
-                console.log("Error Writing OverFile");
-              })
+//Write :
+function PromisifiedWriteFile(filename, data, encoding){
+    return  new Promise(function(resolve,reject){
+        fs.writeFile(filename, data, encoding, function(err){
+            if(err){
+                reject(err);
+            }else{
+                resolve("Changes SucessFully Applied Over the File");
+            }
+        })
+    })
+}
+
+// Changes :
+
+function PromisifiedChanges(filename,encoding){
+    return PromisifiedReadFile(filename, encoding)
+                 .then(function(content){
+                    let contents = content.toUpperCase();
+                    return PromisifiedWriteFile(filename, contents, "utf-8");
+                 })
+}
+
+PromisifiedChanges("main.txt","utf-8")
+               .then(function(message){
+                console.log(message)
+               })
+               .catch(function(err){
+                console.log(err);
+               })
